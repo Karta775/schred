34f0b3b5b8200c8e05fb835b72577b10ab0202e4
Karta775 <Karta775@users.noreply.github.com>
2023-08-09 17:17:28 +0100

    Added clap argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,46 @@
 use schred_lib::*;
+use clap::Parser;
 use std::path::Path;
-use std::env;
+
+/// Secure erase files and directories
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Path to erase
+    path: String,
+
+    /// Print verbose output
+    #[arg(short, long)]
+    verbose: bool,
+
+    /// Delete the file(s) after erasure
+    #[arg(short, long)]
+    deallocate: bool,
+
+    /// Recursive shredding of directories
+    #[arg(short, long)]
+    recursive: bool,
+
+    /// Number of passes using random data
+    #[arg(long, default_value_t = 2)]
+    nrand: u8,
+
+    /// Number of passes using zeros
+    #[arg(long, default_value_t = 1)]
+    nzero: u8,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        eprintln!("You must provide a path to shred!");
-    }
+    let args = Args::parse();
 
     let s = Shredder::new(ShredOptions {
-        recursive: true,
-        deallocate: true,
+        verbose: args.verbose,
+        deallocate: args.deallocate,
+        recursive: args.recursive,
+        rand_passes: args.nrand,
+        zero_passes: args.nzero,
         ..ShredOptions::default()
     });
 
-    s.shred(Path::new(&args[1])).unwrap();
+    s.shred(Path::new(&args.path)).unwrap();
 }
